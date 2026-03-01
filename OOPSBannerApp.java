70bf760219d46887f3a486c4331fe0be9e18b0ef
nm2562 <nm2562@srmist.edu.in>
2026-03-02 01:02:10 +0530

    coded UC5 in console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,16 @@
-class OOPSBannerApp{
-void  main(){
-        System.out.println(String.join("", " ***     ***    *****    ***** "));
-        System.out.println(String.join("", "** **   ** **   **  **   **  **"));
-        System.out.println(String.join("", "**  ** **  **   **  **   **  **"));
-        System.out.println(String.join("", "**   ***   **   *****    ***** "));
-        System.out.println(String.join("", "**    *    **   **       **    "));
-        System.out.println(String.join("", "**         **   **       **    "));
-        System.out.println(String.join("", "**         **   **       **    "));
-    }
+public class OOPSBannerApp {
+	public static void  main(String[] args) {
+		String[] lines = {
+			String.join(" ", "   ***   ", "   ***   ", "*******  ", "   ****  "),
+			String.join(" ", " **   ** ", " **   ** ", "**     **", " **    **"),
+			String.join(" ", "**     **", "**     **", "**     **", "**       "),
+			String.join(" ", "**     **", "**     **", "*******  ", "  *****  "),
+			String.join(" ", "**     **", "**     **", "**       ", "       **"),
+			String.join(" ", " **   ** ", " **   ** ", "**       ", "**    ** "),
+			String.join(" ", "   ***   ", "   ***   ", "**       ", "  ****   ")
+		};
+		for (String line:lines) {
+			System.out.println(line);
+		}
+	}
 }
